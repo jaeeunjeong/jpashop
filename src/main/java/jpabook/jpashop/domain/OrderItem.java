af23c8fc474b51d484230eeafbb9a966c2327ec0
jaeeunjeong <jejeong000@gmail.com>
2022-09-30 08:09:44 +0900

    study : jpa를 이용하여 엔티티를 맵핑하고 h2에서 확인한다. + 네이밍 컨벤션 - 데이터 중심의 설계 방식으로 진행

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long memberId;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    private int price;
+
+    private Long count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+}
